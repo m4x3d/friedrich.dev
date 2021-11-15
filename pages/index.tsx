1f3d1c9aadbb336261acb2054ced34d752cd7498
Max Friedrich <max@friedrich.dev>
2021-11-15 23:04:56 +0100

    refactor: styling of landing page and links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,15 @@
 import { Heading, HStack, Text } from '@chakra-ui/layout';
 import { Link } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import React from 'react';
 import { PageLayout } from '../components/Layout/PageLayout';
 import { SocialMediaLinks } from '../components/SocialMediaLinks';
 
 export const Home: NextPage = () => (
   <PageLayout>
-    <Heading pt="5rem">Hey!</Heading>
-    <HStack p="2rem">
+    <Heading pt="20vh">Hey there!</Heading>
+    <HStack p="2rem" minW="28rem">
       <Text>My name is Max. I'm a Web Developer at</Text>
-      <Link href="https://www.incloud.de/" fontWeight="bold">
-        INCLOUD
-      </Link>
+      <Link href="https://www.incloud.de/">INCLOUD</Link>
       <Text marginInlineStart="0 !important">.</Text>
     </HStack>
     <SocialMediaLinks />
