6ddd9ba8b1d56e416a33747da5c65d94e4f22d3c
Max Friedrich <max@friedrich.dev>
2021-11-15 23:06:03 +0100

    refactor: hidden link and add error feedback

---
+++ components/HiddenEmail.tsx
@@ -0,0 +1,37 @@
+import { Button, Link, Popover, PopoverContent, PopoverTrigger, useToast } from '@chakra-ui/react';
+import React, { FunctionComponent, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+export const HiddenEmail: FunctionComponent = () => {
+  const toast = useToast();
+  const [isMailVisible, setIsMailVisible] = useState<boolean>(false);
+
+  return (
+    <>
+      {!isMailVisible && (
+        <Popover>
+          <PopoverTrigger>
+            <Button size="sm">anzeigen</Button>
+          </PopoverTrigger>
+          <PopoverContent w="304px" h="78px">
+            <ReCAPTCHA
+              sitekey="6LdKLDUdAAAAAGZ-UY6V7p2WKfNa74Id1J2bhdrv"
+              onChange={() => setIsMailVisible(true)}
+              onExpired={() => setIsMailVisible(false)}
+              onErrored={() =>
+                toast({
+                  title: 'reCAPTCHA error',
+                  description: 'Please check your network connection.',
+                  status: 'error',
+                  duration: 10000,
+                  isClosable: true,
+                })
+              }
+            />
+          </PopoverContent>
+        </Popover>
+      )}
+      {isMailVisible && typeof window !== 'undefined' && <Link href="mailto:max@friedrich.dev">max@friedrich.dev</Link>}
+    </>
+  );
+};
