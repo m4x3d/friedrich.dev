117fa571c4be1710068121662879e296743127e2
Max Friedrich <max@friedrich.dev>
2021-12-24 17:08:52 +0100

    feat: add basic 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import { ArrowLeftIcon } from '@chakra-ui/icons';
+import { Button, Heading, HStack, Text } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import { PageLayout } from '../components/Layout/PageLayout';
+
+export const NotFoundPage: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <PageLayout>
+      <Heading pt="20vh">404 - Page not found :(</Heading>
+      <Button onClick={() => router.back()}>
+        <HStack spacing="1rem">
+          <ArrowLeftIcon />
+          <Text>Go Back</Text>
+        </HStack>
+      </Button>
+    </PageLayout>
+  );
+};
+
+export default NotFoundPage;
