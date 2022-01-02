29d2ae205d469a1fe85e9c1e06cff9db8ea91a2b
Max Friedrich <max@friedrich.dev>
2022-01-02 23:41:44 +0100

    fix: adjust link fontSize to text fontSize

--- utils/theme/components/link.ts
+++ utils/theme/components/link.ts
@@ -2,6 +2,7 @@ import type { ComponentStyleConfig } from '@chakra-ui/react';
 
 export const Link: ComponentStyleConfig = {
   baseStyle: {
+    fontSize: '1.125rem',
     fontWeight: 'bold',
     _hover: { textDecoration: 'none' },
   },
