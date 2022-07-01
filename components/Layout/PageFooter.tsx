961a0d1a3d5eeabeb81c6d254ce45d27a7dfeb9c
Max Friedrich <max@friedrich.dev>
2022-07-01 17:18:12 +0200

    refactor: reorder social links

--- components/Layout/PageFooter.tsx
+++ components/Layout/PageFooter.tsx
@@ -1,10 +1,7 @@
-import { HStack, Icon, Link, Text, VStack } from '@chakra-ui/react';
+import { HStack, Link, Text, VStack } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import type { FunctionComponent } from 'react';
-import { FaHeart } from 'react-icons/fa';
 import { ChakraLogo } from '../ChakraLogo';
-
-const FooterDivider = () => <Text px="0.5rem">|</Text>;
 
 export const PageFooter: FunctionComponent = () => (
   <VStack pos="absolute" bottom="1rem" w="full">
@@ -13,10 +10,8 @@ export const PageFooter: FunctionComponent = () => (
       <Link href="https://chakra-ui.com/">
         <ChakraLogo cursor="pointer" h="1.25rem" pb="0.05rem" />
       </Link>
-      <Text>and</Text>
-      <Icon as={FaHeart} pb="0.1rem" />
     </HStack>
-    <HStack divider={<FooterDivider />}>
+    <HStack divider={<Text px="0.5rem">|</Text>}>
       <Text>&copy; 2021 - {new Date().getFullYear()}</Text>
       <Text fontWeight="bold">
         <Link as={NextLink} href="/">
--- components/SocialMediaLinks.tsx
+++ components/SocialMediaLinks.tsx
@@ -1,27 +1,24 @@
 import { HStack, Icon, Link, LinkProps } from '@chakra-ui/react';
 import type { FunctionComponent } from 'react';
 import { FaGitlab, FaXing } from 'react-icons/fa';
-import { GrGithub, GrLinkedinOption, GrMedium, GrTwitter } from 'react-icons/gr';
+import { GrGithub, GrLinkedinOption, GrTwitter } from 'react-icons/gr';
 
 export const SocialMediaLinks: FunctionComponent<LinkProps> = ({
   h = '2rem',
   boxSize = '2rem',
 }) => (
   <HStack spacing="2rem">
-    <Link href="https://gitlab.com/m4x3d" h={h}>
-      <Icon as={FaGitlab} boxSize={boxSize} />
-    </Link>
     <Link href="https://github.com/m4x3d" h={h}>
       <Icon as={GrGithub} boxSize={boxSize} />
     </Link>
-    <Link href="https://medium.com/@m4x3d" h={h}>
-      <Icon as={GrMedium} boxSize={boxSize} />
-    </Link>
-    <Link href="https://www.xing.com/profile/Max_Friedrich46/cv" h={h}>
-      <Icon as={FaXing} boxSize={boxSize} />
+    <Link href="https://gitlab.com/m4x3d" h={h}>
+      <Icon as={FaGitlab} boxSize={boxSize} />
     </Link>
     <Link href="https://www.linkedin.com/in/max-friedrich-119852206/" h={h}>
       <Icon as={GrLinkedinOption} boxSize={boxSize} />
+    </Link>
+    <Link href="https://www.xing.com/profile/Max_Friedrich46/cv" h={h}>
+      <Icon as={FaXing} boxSize={boxSize} />
     </Link>
     <Link href="https://twitter.com/m4x3d_" h={h}>
       <Icon as={GrTwitter} boxSize={boxSize} />
