089fc26b8cbacaa958dd480fa4a51af60e9c424a
Max Friedrich <max@friedrich.dev>
2022-11-15 23:09:05 +0100

    feat: add cookie banner

--- components/Head.tsx
+++ components/Head.tsx
@@ -3,12 +3,18 @@ import type { FunctionComponent } from 'react';
 
 export const Head: FunctionComponent = () => (
   <NextHead>
+    <script
+      id="Cookiebot"
+      src="https://consent.cookiebot.com/uc.js"
+      data-cbid="52df5855-d487-436a-9979-431d1a967c8f"
+      data-blockingmode="auto"
+      type="text/javascript"
+    ></script>
     <title>Max Friedrich</title>
     <meta
       name="description"
       content="Max Friedrich - Web Developer using React, TypeScript and Node.js"
     />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
   </NextHead>
 );
