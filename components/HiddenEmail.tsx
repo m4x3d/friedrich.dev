2d2f1bc3760a1f0dd006ff848f9b2ca0d42865c3
Max Friedrich <max@friedrich.dev>
2022-11-16 22:00:52 +0100

    fix(privacy): remove recaptcha

--- components/HiddenEmail.tsx
+++ components/HiddenEmail.tsx
@@ -1,40 +1,16 @@
-import { Button, Link, Popover, PopoverContent, PopoverTrigger, useColorMode, useToast } from '@chakra-ui/react';
-import type { FunctionComponent } from 'react';
-import { useState } from 'react';
-import ReCAPTCHA from 'react-google-recaptcha';
+import { Button, Link } from '@chakra-ui/react';
+import { useState, type FunctionComponent } from 'react';
 
 export const HiddenEmail: FunctionComponent = () => {
-  const toast = useToast();
   const [isMailVisible, setIsMailVisible] = useState<boolean>(false);
-  const { colorMode } = useColorMode();
+
+  if (isMailVisible && typeof window !== 'undefined') {
+    return <Link href="mailto:max@friedrich.dev">max@friedrich.dev</Link>;
+  }
 
   return (
-    <>
-      {!isMailVisible && (
-        <Popover>
-          <PopoverTrigger>
-            <Button size="sm">anzeigen</Button>
-          </PopoverTrigger>
-          <PopoverContent w="304px" h="78px">
-            <ReCAPTCHA
-              sitekey="6LdKLDUdAAAAAGZ-UY6V7p2WKfNa74Id1J2bhdrv"
-              theme={colorMode}
-              onChange={() => setIsMailVisible(true)}
-              onExpired={() => setIsMailVisible(false)}
-              onErrored={() =>
-                toast({
-                  title: 'reCAPTCHA error',
-                  description: 'Please check your network connection.',
-                  status: 'error',
-                  duration: 10000,
-                  isClosable: true,
-                })
-              }
-            />
-          </PopoverContent>
-        </Popover>
-      )}
-      {isMailVisible && typeof window !== 'undefined' && <Link href="mailto:max@friedrich.dev">max@friedrich.dev</Link>}
-    </>
+    <Button size="sm" onClick={() => setIsMailVisible(true)}>
+      anzeigen
+    </Button>
   );
 };
