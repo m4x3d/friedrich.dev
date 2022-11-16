c46e5e51684b313ef5cedd698276e3e5fdb8ae5a
Max Friedrich <max@friedrich.dev>
2022-11-16 23:35:03 +0100

    refactor: update intro text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { Heading, Link, Text } from '@chakra-ui/react';
-import type { NextPage } from 'next';
+import { Heading, Highlight, Text } from '@chakra-ui/react';
+import { type NextPage } from 'next';
 import { PageLayout } from '../components/Layout/PageLayout';
 import { SocialMediaLinks } from '../components/SocialMediaLinks';
 
@@ -7,7 +7,16 @@ export const Home: NextPage = () => (
   <PageLayout>
     <Heading pt="15vh">Hey there!</Heading>
     <Text p="2rem">
-      My name is Max. I'm a Web Developer at <Link href="https://www.incloud.de/">Incloud</Link>.
+      <Highlight
+        query={['frontends', 'react', 'typescript', 'serverless backends', 'aws', 'terraform']}
+        styles={{
+          textDecoration: 'underline double',
+          color: 'inherit',
+        }}
+      >
+        My name is Max. I'm a Web Developer with a focus on Frontends built with React and
+        TypeScript as well as serverless backends with AWS and Terraform.
+      </Highlight>
     </Text>
     <SocialMediaLinks />
   </PageLayout>
