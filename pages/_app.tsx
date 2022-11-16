5d59b064c28ef013fbc8950b4c4e14274bcfcf07
Max Friedrich <max@friedrich.dev>
2022-11-16 23:50:45 +0100

    refactor(theme): simplify initial color mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,15 @@
-import { ChakraProvider, ColorModeOptions, ColorModeProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/work-sans/400.css';
 import '@fontsource/work-sans/500.css';
 import type { AppProps } from 'next/app';
 import { Head } from '../components/Head';
 import theme from '../utils/theme/theme';
 
-const options: ColorModeOptions = {
-  initialColorMode: 'dark',
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <ColorModeProvider options={options}>
-        <Head />
-        <Component {...pageProps} />
-      </ColorModeProvider>
+      <Head />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
--- utils/theme/theme.ts
+++ utils/theme/theme.ts
@@ -4,6 +4,7 @@ import { fonts } from './fonts';
 
 const theme = extendTheme({
   fonts,
+  initialColorMode: 'dark',
   components: {
     Heading,
     Link,
