eb537461e703bcc5aa70a7cbbd91b1ff0867cc7b
Max Friedrich <max@friedrich.dev>
2022-11-25 21:11:39 +0100

    refactor: cleanup social links

--- components/SocialMediaLinks.tsx
+++ components/SocialMediaLinks.tsx
@@ -1,27 +1,30 @@
 import { HStack, Icon, Link, LinkProps } from '@chakra-ui/react';
-import type { FunctionComponent } from 'react';
-import { FaGitlab, FaXing } from 'react-icons/fa';
+import type { FunctionComponent, PropsWithChildren } from 'react';
+import { BsStrava } from 'react-icons/bs';
 import { GrGithub, GrLinkedinOption, GrTwitter } from 'react-icons/gr';
 
-export const SocialMediaLinks: FunctionComponent<LinkProps> = ({
-  h = '2rem',
-  boxSize = '2rem',
+const SocialMediaLink: FunctionComponent<PropsWithChildren<LinkProps>> = ({
+  children,
+  ...rest
 }) => (
-  <HStack spacing="2rem">
-    <Link href="https://github.com/m4x3d" h={h}>
+  <Link h="8" target="_blank" {...rest}>
+    {children}
+  </Link>
+);
+
+export const SocialMediaLinks: FunctionComponent<LinkProps> = ({ h, boxSize = 8 }) => (
+  <HStack spacing="8">
+    <SocialMediaLink href="https://github.com/m4x3d" h={h}>
       <Icon as={GrGithub} boxSize={boxSize} />
-    </Link>
-    <Link href="https://gitlab.com/m4x3d" h={h}>
-      <Icon as={FaGitlab} boxSize={boxSize} />
-    </Link>
-    <Link href="https://www.linkedin.com/in/max-friedrich-119852206/" h={h}>
+    </SocialMediaLink>
+    <SocialMediaLink href="https://www.linkedin.com/in/max-friedrich-119852206/" h={h}>
       <Icon as={GrLinkedinOption} boxSize={boxSize} />
-    </Link>
-    <Link href="https://www.xing.com/profile/Max_Friedrich46/cv" h={h}>
-      <Icon as={FaXing} boxSize={boxSize} />
-    </Link>
-    <Link href="https://twitter.com/m4x3d_" h={h}>
+    </SocialMediaLink>
+    <SocialMediaLink href="https://twitter.com/m4x3d_" h={h}>
       <Icon as={GrTwitter} boxSize={boxSize} />
-    </Link>
+    </SocialMediaLink>
+    <SocialMediaLink href="https://strava.com/athletes/m4x_d" h={h}>
+      <Icon as={BsStrava} boxSize={boxSize} />
+    </SocialMediaLink>
   </HStack>
 );
