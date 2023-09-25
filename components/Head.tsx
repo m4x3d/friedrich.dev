d1cbd6e0720cdc3b99bf6a62c4383811d4c85779
Max Friedrich <max@friedrich.dev>
2023-09-25 19:15:52 +0200

    chore: remove cookiebot

--- components/Head.tsx
+++ components/Head.tsx
@@ -3,17 +3,10 @@ import type { FunctionComponent } from 'react';
 
 export const Head: FunctionComponent = () => (
   <NextHead>
-    <script
-      id="Cookiebot"
-      src="https://consent.cookiebot.com/uc.js"
-      data-cbid="52df5855-d487-436a-9979-431d1a967c8f"
-      data-blockingmode="auto"
-      type="text/javascript"
-    ></script>
     <title>Max Friedrich</title>
     <meta
       name="description"
-      content="Max Friedrich - Web Developer using React, TypeScript and Node.js"
+      content="Max Friedrich - Web Developer using React, TypeScript and AWS"
     />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   </NextHead>
